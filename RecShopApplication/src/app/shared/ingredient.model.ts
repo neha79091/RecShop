8ac80762b7a1c7e90b6c03286190a20057d0991b
neha79091 <neha79091@gmail.com>
2022-01-05 14:03:52 +0530

    basicStructureApplication

---
+++ RecShopApplication/src/app/shared/ingredient.model.ts
@@ -0,0 +1,7 @@
+export class Ingredient {
+    //this code will make two properties name and amount and will assign values which we pass in arguements
+    constructor(public name:string,public amount:number)
+    {
+
+    }
+}
